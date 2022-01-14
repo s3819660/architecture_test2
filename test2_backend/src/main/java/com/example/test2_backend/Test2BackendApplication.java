41ad4ebeac4e036b055455e31b5cb41274544241
s3819660 <56669093+s3819660@users.noreply.github.com>
2022-01-14 09:24:17 +0700

    first commit

---
+++ test2_backend/src/main/java/com/example/test2_backend/Test2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.test2_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test2BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test2BackendApplication.class, args);
+    }
+
+}
---
+++ test2_backend/src/test/java/com/example/test2_backend/Test2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test2_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test2BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
